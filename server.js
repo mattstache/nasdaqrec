aa8f5e9ada8aa2e48f141b98e0cb06f345719c0c
Matt <mattalarie@gmail.com>
2017-10-13 11:28:40 -0700

    Remove old unused comment from server.js

--- server.js
+++ server.js
@@ -26,7 +26,9 @@ var promise = mongoose.connect(db, {
 promise.then(function(db) {
 
 
-
+	if (app.get('env') === 'development') var dev = true;
+	// logger in dev mode (morgan)
+	//if (dev) app.use(logger('dev'));
 
 	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use(bodyParser.json());
@@ -40,7 +42,7 @@ promise.then(function(db) {
 	//and remove cacheing so we get the most recent comments
 	 res.setHeader('Cache-Control', 'no-cache');
 	 next();
-	});
+	}); 
 
 	//now we can set the route path & initialize the API
 	//Define our routes
@@ -75,70 +77,3 @@ promise.then(function(db) {
 	});
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-//models
-//const List = require('./src/app/model/List.model');
-
-
-//controller
-//const UserHandler = require('./src/app/controllers/user.controller');
-
-//set our port to either a predetermined port number if you have set 
-//it up, or 3001
-// var port = process.env.API_PORT || 3001;
-// const db = 'mongodb://localhost/mongoosetest';
-// mongoose.Promise = global.Promise;
-
-
-// // Using `mongoose.connect`...
-// var promise = mongoose.connect(db, {
-//   useMongoClient: true,
-//   /* other options */
-// });
-
-// promise.then(function(db) {
-//   /* Use `db`, for instance `db.model()`*/
-
-//   	//now we should configure the API to use bodyParser and look for 
-// 	//JSON data in the request body
-// 	app.use(bodyParser.urlencoded({ extended: true }));
-// 	app.use(bodyParser.json());
-// 	//To prevent errors from Cross Origin Resource Sharing, we will set 
-// 	//our headers to allow CORS with middleware like so:
-// 	app.use(function(req, res, next) {
-// 	 res.setHeader('Access-Control-Allow-Origin', '*');
-// 	 res.setHeader('Access-Control-Allow-Credentials', 'true');
-// 	 res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-// 	 res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-// 	//and remove cacheing so we get the most recent comments
-// 	 res.setHeader('Cache-Control', 'no-cache');
-// 	 next();
-// 	});
-
-// 	//now we can set the route path & initialize the API
-// 	//Define our routes
-// 	var listRoutes = require('./src/app/routes/listRoutes');
-// 	var authRoutes = require('./src/app/routes/authRoutes');
-
-// 	//Use our router configuration when we call /api
-// 	app.use('/api/list', listRoutes);
-// 	app.use('/api/auth', authRoutes);
-
-// 	//starts the server and listens for requests
-// 	app.listen(port, function() {
-// 	 console.log(`API running on port ${port}`);
-// 	});
-// });
-
