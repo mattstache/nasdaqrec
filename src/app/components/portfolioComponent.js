571b22462131dbdc4b153b963ce784b1cfc3e333
Matt <mattalarie@gmail.com>
2017-10-25 10:15:11 -0700

    Add missing portfolioComponent file

---
+++ src/app/components/portfolioComponent.js
@@ -0,0 +1,20 @@
+var React = require('react');
+
+//Create PortfolioComponent
+class PortfolioComponent extends React.Component{
+	constructor(){
+        super();
+    }
+
+	render(){
+		return(
+			<div>
+				Portfolio
+			</div>
+		);
+	}
+
+
+};
+
+export default PortfolioComponent;
