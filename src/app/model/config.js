f0f0483ed0a230e12088aef4163ac43a4b751da7
Matt <mattalarie@gmail.com>
2017-11-29 10:01:15 -0800

    Remove old config files

--- src/app/model/config.js
+++ src/app/model/config.js
@@ -1,12 +1,3 @@
 module.exports = {
-	port: 3001,
-	apiUrl: 'http://localhost:3001',
-	dbUrl: 'mongodb://localhost:5000/nasdaqrec',
 
-	//secret for hashing tokens (randomly generated)
-	secret: '37db92dce80c468c95b72248c1d49794',
-
-	//initial admin email
-	adminEmail: 'admin@admin.com',
-	adminPassword: 'test'
 };
