fdc15ab4a6a855e7224f9d7496c6632b297e15a0
Matt <mattalarie@gmail.com>
2017-11-29 14:05:00 -0800

    Rollback wildcard route

--- server.js
+++ server.js
@@ -83,9 +83,9 @@ promise.then(function(db) {
 
 	app.use(express.static(__dirname + "/dist"));
 
-	app.get('/*', function (req, res) {
-	   res.sendFile(path.join(__dirname + "/dist", 'index.html'));
-	 });
+	// app.get('/*', function (req, res) {
+	//    res.sendFile(path.join(__dirname + "/dist", 'index.html'));
+	//  });
 
 
 
